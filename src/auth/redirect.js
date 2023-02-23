4de1771935d6c651726fd30fa779ddabc69743ab
danial <danial@danials.space>
2023-02-23 10:25:33 +0330

    fix security issue in redirect page

--- src/auth/redirect.js
+++ src/auth/redirect.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+const { URL } = require('url');
 
 function RedirectPage({ match }) {
   useEffect(() => {
@@ -6,15 +7,31 @@ function RedirectPage({ match }) {
     const urlParams = new URLSearchParams(window.location.search);
     const redirectParam = urlParams.get("redirect");
 
-    // Get user's accessToken
-    const accessToken = localStorage.getItem('accessToken');
+    // check if its a valid url
+    try {
+      const urlObject = new URL(redirectParam);
+      const domainName = 'danials.space';
+      
+      // check if the url ends with *.danials.space
+      if (urlObject.hostname.endsWith(domainName)) {
 
-    if (accessToken && redirectParam) {
-      // Redirect to the link with token as param
-      const redirectLink = `${redirectParam}?token=${accessToken}`;
-      window.location.href = redirectLink;
-    } else {
-      // Redirect to main page if user is not logged in
+        // Get user's accessToken
+        const accessToken = localStorage.getItem('accessToken');
+
+        if (accessToken && redirectParam) {
+          // Redirect to the link with token as param
+          const redirectLink = `${redirectParam}?token=${accessToken}`;
+          window.location.href = redirectLink;
+        } else {
+          // Redirect to main page if user is not logged in
+          window.location.href = "/";
+        }
+        } else {
+        console.log('The URL is valid but does not end with ' + domainName);
+        window.location.href = "/";
+      }
+    } catch (err) {
+      console.error('The URL is not valid:', err);
       window.location.href = "/";
     }
   }, []);
